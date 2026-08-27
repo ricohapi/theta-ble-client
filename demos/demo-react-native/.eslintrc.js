a087dc22028cddf50cdcae78345a4aa76597640a
松岡　侑凛 <129148471+LassicYM@users.noreply.github.com>
2026-08-27 09:24:36 +0900

    update react-native demo.

--- demos/demo-react-native/.eslintrc.js
+++ demos/demo-react-native/.eslintrc.js
@@ -1,21 +1,4 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  env: {
-    es2019: true,
-    node: true,
-  },
-  rules: {
-    semi: ['error', 'always'],
-    quotes: ['warn', 'single'],
-    indent: ['error', 2],
-    'comma-dangle': ['error', 'always-multiline'],
-    eqeqeq: ['error', 'always', { null: 'ignore' }],
-    'object-curly-newline': [
-      'error',
-      { multiline: true, minProperties: 80, consistent: true },
-    ],
-    'object-curly-spacing': ['error', 'always'],
-    'react-native/no-inline-styles': 0,
-  },
+  extends: '@react-native',
 };
